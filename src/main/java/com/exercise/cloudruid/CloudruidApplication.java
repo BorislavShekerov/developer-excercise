513e44851ec063c17368b4aebd031e197c87b613
Radoslav <ketchup4eto@abv.bg>
2023-03-11 01:34:53 +0200

    Project setup

---
+++ src/main/java/com/exercise/cloudruid/CloudruidApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.cloudruid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudruidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exercise/cloudruid/CloudruidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.cloudruid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudruidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
