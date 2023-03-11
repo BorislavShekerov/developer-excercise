9cad817bc3cb873bd5bc476d8e799ad26d366a14
Radoslav <ketchup4eto@abv.bg>
2023-03-11 02:45:06 +0200

    Added Groceries model and dto

---
+++ src/main/java/com/exercise/cloudruid/models/Groceries.java
@@ -0,0 +1,28 @@
+package com.exercise.cloudruid.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Setter;
+
+@Data
+@Entity(name = "Groceries")
+@Table(name = "groceries")
+public class Groceries {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private @Setter double price;
+
+    @Column(name = "2_for_3")
+    private @Setter boolean twoForThree;
+
+    @Column(name = "buy_1_get_1_half_price")
+    private @Setter boolean buyOneGetOneHalfPrice;
+}
---
+++ src/main/java/com/exercise/cloudruid/models/dtos/GroceriesDto.java
@@ -0,0 +1,13 @@
+package com.exercise.cloudruid.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+public class GroceriesDto {
+
+    private String name;
+
+    private double price;
+}
