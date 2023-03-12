f0f2315f9266036d95a5c0d341ca897b7a1ccb02
Radoslav <ketchup4eto@abv.bg>
2023-03-12 14:11:35 +0200

    Added annotations to prevent incorrect input and price field is changed to a string

---
+++ src/main/java/com/exercise/cloudruid/utils/dtos/GroceriesDto.java
@@ -0,0 +1,20 @@
+package com.exercise.cloudruid.utils.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class GroceriesDto {
+
+    @NotEmpty(message = "Product name cannot be empty!")
+    private String name;
+
+    @NotEmpty (message = "Price cannot be empty!")
+    private String price;
+
+}
